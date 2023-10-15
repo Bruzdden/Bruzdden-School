bff254b1c75389aa86a50244d2eead91f5342577
Lukáš Divíšek <76947123+Bruzdden@users.noreply.github.com>
2023-10-15 15:11:16 +0200

    Create analyze.py

---
+++ python/analyze.py
@@ -0,0 +1,18 @@
+graph = [[0, 1, 0, 1, 0],
+         [1, 0, 1, 1, 1],
+         [0, 1, 0, 1, 0],
+         [1, 1, 1, 0, 0],
+         [0, 1, 0, 0, 0],]
+
+def analyze(graph):
+    print("Uzlů v grafu je: ", len(graph))
+    
+    for i in range(len(graph)):
+        for j in range(i):
+            if graph[i][j] == 1 and graph[j][i] == 1:
+                print("Uzli ",i+1," a ",j+1," jsou propojené")
+            else:
+                print("Uzli ",i+1," a ",j+1," nejsou propojené")
+
+
+analyze(graph)
